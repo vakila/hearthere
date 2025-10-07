0adb05684a8225c72c3a6c290c99770f6f5d4355
Anjana Vakil <contact@anjana.dev>
2025-10-07 12:53:15 -0400

    polyphonic F chord

--- src/synth.ts
+++ src/synth.ts
@@ -1,14 +1,18 @@
-import { Synth } from "tone";
+import { PolySynth, Synth } from "tone";
 
 //create a synth and connect it to the main output (your speakers)
-export const synth = new Synth().toDestination();
+export const monosynth = new Synth().toDestination();
+const synth = new PolySynth(Synth).toDestination();
+
+const Fmaj = ["F3", "A3", "C3"]
+
 
 export function play() {
     //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttack("C4");
+    synth.triggerAttack(Fmaj);
 }
 
 export function pause() {
     //release the note immediately
-    synth.triggerRelease();
+    synth.triggerRelease(Fmaj);
 }
