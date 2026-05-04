ce861c8384e68c5d4825d6cfa45ce76db3a0dc8f
Anjana Vakil <contact@anjana.dev>
2026-05-04 18:48:44 +0200

    trying to get gh pages working

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/hearthere/",
   build: {
     sourcemap: true,
   },
   dev: {
     sourcemap: true,
-  }
-})
+  },
+});
